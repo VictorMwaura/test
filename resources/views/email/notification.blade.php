607a474082035a79014a7a6089a22722e1476081
VictorMwaura <71704946+VictorMwaura@users.noreply.github.com>
2023-07-13 17:16:53 +0300

    Create notification.blade.php

---
+++ resources/views/email/notification.blade.php
@@ -0,0 +1,56 @@
+<x-app-layout>
+  <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Hello Admin, Notify Users') }}
+        </h2>
+    </x-slot>
+
+ <x-guest-layout>
+        <html>
+        <head>
+    <style>
+            .card {
+                position: relative;
+                width: 350px;
+                height: 450px;
+                margin: 30px;
+                box-shadow: 20px 20px 50px rgba(0, 0, 0, 0.5);
+                border-radius: 15px;
+                background: rgba(255, 255, 255, 0.1);
+                overflow: hidden;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                border-top: 1px solid rgba(255, 255, 255, 0.5);
+                border-left: 1px solid rgba(255, 255, 255, 0.5);
+                backdrop-filter: blur(5px);
+                margin:auto;
+                margin-top:10px;
+            }
+            
+        </style>
+    </head>
+   <body> 
+       <div class="card">
+   <form action="{{  route('testenrollment')  }}" method="post" >
+       
+   @csrf
+<div class="mt-4">
+    <label class='block font-medium text-sm text-gray-700'>welcome: </label><input type="text" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="welcome">
+</div>
+<div class="mt-4">
+    <label class='block font-medium text-sm text-gray-700'>notification:</label> <input type="text" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name = "notification"> 
+</div>
+<div class="mt-4">
+    <label class='block font-medium text-sm text-gray-700'>close:</label> <input type="text" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="close">
+</div>
+<x-button class="mt-4 ml-4 mr-4 ">
+                   Notify
+                </x-button>
+        
+</form>
+        </div>
+        </body>
+        </html>
+        </x-guest-layout>
+ </x-app-layout>
